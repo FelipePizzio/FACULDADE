c8486ef446dd286b2c0f5cb2e785dff9e83efbe4
Felipe Pizzio <felipetpizzio@gmail.com>
2022-05-18 21:53:28 -0300

    exemplo de health check

---
+++ health-check/src/main/java/org/acme/MyReadinessCheck.java
@@ -0,0 +1,18 @@
+package org.acme;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Readiness;
+
+@Readiness
+public class MyReadinessCheck implements HealthCheck {
+    
+    @Override
+    public HealthCheckResponse call() {
+        HealthCheckResponseBuilder response = HealthCheckResponse.named("Database connection health check");
+        response.up().withData("key", "value");
+
+        return response.build();
+    }
+}
